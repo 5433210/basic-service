faa6c4e0e705cae04726ecddd845efe2e0c3d21c
genham <5433210@qq.com>
2023-03-27 01:18:22 +0800

    build mqtt_sender first before build sched docker image

--- build/package/sched/build.sh
+++ build/package/sched/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ${ROOT_DIR}
-make go.build.linux_amd64.sched
+make go.build.linux_amd64.mqtt_sender
 cp ${OUTPUT_DIR}/platforms/${IMAGE_PLAT}/mqtt_sender "${DST_DIR}"
 cp -rv ${CFG_DIR}/sched.yaml "$DST_DIR"
